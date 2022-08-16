212b8e398cd570c51fc5a909d1a28a2375994ac6
Alli Bolade <110709444+gos27@users.noreply.github.com>
2022-08-16 09:15:51 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+    <head>        
+    <meta charset="utf-8"><title>Bolade Alli</title> 
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="favicon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Merriweather&family=Montserrat&family=Sacramento&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="top_container">
+            <img class="top_cloud" src="images/cloud.png" alt="cloud-img">
+            <h1>I'm Bolade.</h1>
+            <h3>a <span class="pro" ><u>pro</u></span>grammer</h3>
+            <img class="bottom_cloud" src="images/cloud.png" alt="cloud-img">
+            <img src="images/mountain.png" alt="mountain">
+        </div>
+        <div class="middle_container">
+            <div class="profile">
+              <img src="images/passport.png" alt="">
+              <h2>Hello.</h2>
+              <p class="intro">I am a Web Developer and I enjoy sports and playing computer game.</p>
+            </div>
+            <hr>
+            <div class="skills">
+              <h2>My Skills.</h2>
+              <div class="skill_row">
+                <img class="computer" src="images/skills.jpg" alt="skill">
+                <h3>Computer</h3>
+                <p> I enjoy computer and its applications on my daily activities and ever since I start coding it has being a real good part of my live.
+                </p>
+              </div>
+              <div class="skill_row">
+                <img class="fun" src="images/fun.png" alt="fun">
+                <h3>Outdoor</h3>
+                <p>I do enjoy outdoor activties a lot when I am not busy and mostly over the weekend.</p>
+              </div>
+            </div>
+            <hr>
+            <div class="contact-me">
+              <h2>Get In Touch</h2>
+              <h3>Let us have fun</h3>
+              <p class="contact_message">Just incase you got some plans for work and fun, I want you to know that I will be available for work during the week and maybe hangout over the weekend
+                <span><em>I do enjoy few drinks with friends and family occasionally.</em></span></p>
+              <a class="btn" href="mailto:boladealli27@gmail.com">CONTACT ME</a>
+            </div>
+          </div>
+          
+          
+          
+          <div class="bottom_container">
+        <a class="footer_link" href="callto:+2348072963710"> Call US</a>
+        <a class="footer_link" href="https://twitter.com/gos027">Twitter</a>
+        <a class="footer_link" href="www.linkedin.com/in/gos27">Linkedin</a>
+            <p class="copyright">© <?php echo "2018-".date("Y"); ?> aboladegos.</p>
+          </div>
+          
+        <div class="footer">
+
+        </div>
+            
+             
+    </body>
+</html>
